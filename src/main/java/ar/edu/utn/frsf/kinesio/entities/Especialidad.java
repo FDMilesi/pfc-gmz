756ae7e9552a0649d853bf8b1b6421566c91c00c
fzen2016 <fco.zen@gmail.com>
2016-02-02 20:22:43 -0300

    Se mapean las entidades: Tratamiento, TipoDeTratamiento, TipoTratamientoObraSocial y Especialidad. Se corrigió el nombre de la entidad ObraSocial.

---
+++ src/main/java/ar/edu/utn/frsf/kinesio/entities/Especialidad.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ar.edu.utn.frsf.kinesio.entities;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author Fran
+ */
+@Entity
+@Table(name = "especialidad")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Especialidad.findAll", query = "SELECT e FROM Especialidad e"),
+    @NamedQuery(name = "Especialidad.findById", query = "SELECT e FROM Especialidad e WHERE e.id = :id"),
+    @NamedQuery(name = "Especialidad.findByNombre", query = "SELECT e FROM Especialidad e WHERE e.nombre = :nombre")})
+public class Especialidad implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Short id;
+    @Size(max = 30)
+    @Column(name = "nombre")
+    private String nombre;
+    @OneToMany(mappedBy = "especialidadid")
+    private List<TipoDeTratamiento> tipoDeTratamientoList;
+
+    public Especialidad() {
+    }
+
+    public Especialidad(Short id) {
+        this.id = id;
+    }
+
+    public Short getId() {
+        return id;
+    }
+
+    public void setId(Short id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @XmlTransient
+    public List<TipoDeTratamiento> getTipoDeTratamientoList() {
+        return tipoDeTratamientoList;
+    }
+
+    public void setTipoDeTratamientoList(List<TipoDeTratamiento> tipoDeTratamientoList) {
+        this.tipoDeTratamientoList = tipoDeTratamientoList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Especialidad)) {
+            return false;
+        }
+        Especialidad other = (Especialidad) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ar.edu.utn.frsf.kinesio.entities.Especialidad[ id=" + id + " ]";
+    }
+    
+}
