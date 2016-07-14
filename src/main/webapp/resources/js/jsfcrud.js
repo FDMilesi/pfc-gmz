9947e0b4aa6897910c9cac92cbcbae728f5d7c1e
FDMilesi <fernando.milesi.8@gmail.com>
2016-07-14 16:13:08 -0300

    En tamaños chicos de pantalla la agenda cambia a Dia. Se agrega tema. Se agrega el tema omega de modo que sea fácilmente editable, llamándose omega2

--- src/main/webapp/resources/js/jsfcrud.js
+++ src/main/webapp/resources/js/jsfcrud.js
@@ -6,3 +6,25 @@ function handleSubmit(args, dialog) {
         PF(dialog).hide();
     }
 }
+
+//Listener de cambio de tamaño
+var mediaMatchMax640 = window.matchMedia("(max-width: 40.063em)");
+function mediaMatchMax640Listener(mediaQuery) {
+    if (mediaQuery.matches) {
+        //más chico que 640
+        setTimeout(function () {
+            if (PrimeFaces.widgets['agenda']) {
+                var fullCal = PF('agenda').jq.children(':first');
+//                fullCal.fullCalendar({header: false});
+                fullCal.fullCalendar('changeView', 'agendaDay');
+                $('.fc-right').css("display", "none");
+            }
+        }, 10);
+    } else {
+        //más grande que 640
+        if (PrimeFaces.widgets['agenda']) {
+            $('.fc-right').css("display", "block");
+        }
+    }
+}
+mediaMatchMax640.addListener(mediaMatchMax640Listener);
