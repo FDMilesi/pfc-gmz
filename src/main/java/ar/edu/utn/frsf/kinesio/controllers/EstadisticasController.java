4f50b81d95b9cda2028a43f386eaa1f5875aff30
fzen2016 <fco.zen@gmail.com>
2017-02-06 19:15:41 -0300

    Estadisticas. Grafico de barra de la cantidad de pacientes.

--- src/main/java/ar/edu/utn/frsf/kinesio/controllers/EstadisticasController.java
+++ src/main/java/ar/edu/utn/frsf/kinesio/controllers/EstadisticasController.java
@@ -13,7 +13,9 @@ import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import org.primefaces.model.chart.Axis;
 import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.BarChartModel;
 import org.primefaces.model.chart.CategoryAxis;
+import org.primefaces.model.chart.ChartSeries;
 import org.primefaces.model.chart.LineChartModel;
 import org.primefaces.model.chart.LineChartSeries;
 import org.primefaces.model.chart.PieChartModel;
@@ -23,6 +25,7 @@ public class EstadisticasController implements Serializable {
 
     private LineChartModel animatedModel1;
     private PieChartModel pieModel;
+    private BarChartModel barModel;
 
     @EJB
     private EstadisticasFacade estadisticasFacade;
@@ -50,7 +53,12 @@ public class EstadisticasController implements Serializable {
         return pieModel;
     }
 
+    public BarChartModel getBarModel() {
+        return barModel;
+    }
+
     private void createAnimatedModels() {
+        //grafico de lineas
         animatedModel1 = initLinearModel();
         animatedModel1.setTitle("Cantidad de pacientes en los últimos meses");
         animatedModel1.setAnimate(true);
@@ -58,13 +66,22 @@ public class EstadisticasController implements Serializable {
         Axis yAxis = animatedModel1.getAxis(AxisType.Y);
         yAxis.setMin(0);
         yAxis.setMax(5);
-        
+
+        //grafico de torta
         pieModel = initPieModel();
         pieModel.setTitle("Obras Sociales mas importantes");
         pieModel.setLegendPosition("e");
 //        pieModel.setFill(false);
         pieModel.setShowDataLabels(true);
         pieModel.setDiameter(150);
+
+        //grafico de barras
+        barModel = initBarModel();
+        barModel.setTitle("Cantidad pacientes por mes");
+        barModel.setAnimate(true);
+        Axis yAxisBarModel = barModel.getAxis(AxisType.Y);
+        yAxisBarModel.setMin(0);
+        yAxisBarModel.setMax(5);
     }
 
     private LineChartModel initLinearModel() {
@@ -117,21 +134,95 @@ public class EstadisticasController implements Serializable {
 
         return model;
     }
-    
-    private PieChartModel initPieModel(){
+
+    private PieChartModel initPieModel() {
         PieChartModel model = new PieChartModel();
         ObraSocial iapos = this.getObraSocialFacade().getObraSocialIAPOS();
         ObraSocial jerarquicos = this.getObraSocialFacade().getObraSocialJerarquicos();
         ObraSocial sancor = this.getObraSocialFacade().getObraSocialSancor();
         ObraSocial ospat = this.getObraSocialFacade().getObraSocialOSPAT();
         ObraSocial uta = this.getObraSocialFacade().getObraSocialUTA();
-        
+
         model.set("Iapos", getFacade().CantidadPacientesConObraSocialX(iapos));
         model.set("Jerarquicos Salud", getFacade().CantidadPacientesConObraSocialX(jerarquicos));
         model.set("Sancor Salud", getFacade().CantidadPacientesConObraSocialX(sancor));
         model.set("OSPAT (Pers. del Turf)", getFacade().CantidadPacientesConObraSocialX(ospat));
         model.set("U.T.A. - O.S.C.T.C.P.", getFacade().CantidadPacientesConObraSocialX(uta));
-        
+
+        return model;
+    }
+
+    private BarChartModel initBarModel() {
+        BarChartModel model = new BarChartModel();
+
+        Calendar c = Calendar.getInstance();
+        int mes1 = c.get(Calendar.MONTH);
+        int anio1 = c.get(Calendar.YEAR);
+        c.add(Calendar.MONTH, -1);
+        int mes2 = c.get(Calendar.MONTH);
+        int anio2 = c.get(Calendar.YEAR);
+        c.add(Calendar.MONTH, -1);
+        int mes3 = c.get(Calendar.MONTH);
+        int anio3 = c.get(Calendar.YEAR);
+        c.add(Calendar.MONTH, -1);
+        int mes4 = c.get(Calendar.MONTH);
+        int anio4 = c.get(Calendar.YEAR);
+        c.add(Calendar.MONTH, -1);
+        int mes5 = c.get(Calendar.MONTH);
+        int anio5 = c.get(Calendar.YEAR);
+        c.add(Calendar.MONTH, -1);
+        int mes6 = c.get(Calendar.MONTH);
+        int anio6 = c.get(Calendar.YEAR);
+        c.add(Calendar.MONTH, -1);
+        int mes7 = c.get(Calendar.MONTH);
+        int anio7 = c.get(Calendar.YEAR);
+        c.add(Calendar.MONTH, -1);
+        int mes8 = c.get(Calendar.MONTH);
+        int anio8 = c.get(Calendar.YEAR);
+        c.add(Calendar.MONTH, -1);
+        int mes9 = c.get(Calendar.MONTH);
+        int anio9 = c.get(Calendar.YEAR);
+        c.add(Calendar.MONTH, -1);
+        int mes10 = c.get(Calendar.MONTH);
+        int anio10 = c.get(Calendar.YEAR);
+        c.add(Calendar.MONTH, -1);
+        int mes11 = c.get(Calendar.MONTH);
+        int anio11 = c.get(Calendar.YEAR);
+        c.add(Calendar.MONTH, -1);
+        int mes12 = c.get(Calendar.MONTH);
+        int anio12 = c.get(Calendar.YEAR);
+
+        String NombreMes1 = Month.values()[mes1].getDisplayName(TextStyle.FULL, new Locale("ES", "AR"));
+        String NombreMes2 = Month.values()[mes2].getDisplayName(TextStyle.FULL, new Locale("ES", "AR"));
+        String NombreMes3 = Month.values()[mes3].getDisplayName(TextStyle.FULL, new Locale("ES", "AR"));
+        String NombreMes4 = Month.values()[mes4].getDisplayName(TextStyle.FULL, new Locale("ES", "AR"));
+        String NombreMes5 = Month.values()[mes5].getDisplayName(TextStyle.FULL, new Locale("ES", "AR"));
+        String NombreMes6 = Month.values()[mes6].getDisplayName(TextStyle.FULL, new Locale("ES", "AR"));
+        String NombreMes7 = Month.values()[mes7].getDisplayName(TextStyle.FULL, new Locale("ES", "AR"));
+        String NombreMes8 = Month.values()[mes8].getDisplayName(TextStyle.FULL, new Locale("ES", "AR"));
+        String NombreMes9 = Month.values()[mes9].getDisplayName(TextStyle.FULL, new Locale("ES", "AR"));
+        String NombreMes10 = Month.values()[mes10].getDisplayName(TextStyle.FULL, new Locale("ES", "AR"));
+        String NombreMes11 = Month.values()[mes11].getDisplayName(TextStyle.FULL, new Locale("ES", "AR"));
+        String NombreMes12 = Month.values()[mes12].getDisplayName(TextStyle.FULL, new Locale("ES", "AR"));
+
+        ChartSeries pacientesPorMes = new ChartSeries();
+        pacientesPorMes.setLabel("pacientesPorMes");
+        pacientesPorMes.set(NombreMes12, this.getFacade().CantidadPacientesPorMesTotal(mes12, anio12));
+        pacientesPorMes.set(NombreMes11, this.getFacade().CantidadPacientesPorMesTotal(mes11, anio11));
+        pacientesPorMes.set(NombreMes10, this.getFacade().CantidadPacientesPorMesTotal(mes10, anio10));
+        pacientesPorMes.set(NombreMes9, this.getFacade().CantidadPacientesPorMesTotal(mes9, anio9));
+        pacientesPorMes.set(NombreMes8, this.getFacade().CantidadPacientesPorMesTotal(mes8, anio8));
+        pacientesPorMes.set(NombreMes7, this.getFacade().CantidadPacientesPorMesTotal(mes7, anio7));
+        pacientesPorMes.set(NombreMes6, this.getFacade().CantidadPacientesPorMesTotal(mes6, anio6));
+        pacientesPorMes.set(NombreMes5, this.getFacade().CantidadPacientesPorMesTotal(mes5, anio5));
+        pacientesPorMes.set(NombreMes4, this.getFacade().CantidadPacientesPorMesTotal(mes4, anio4));
+        pacientesPorMes.set(NombreMes3, this.getFacade().CantidadPacientesPorMesTotal(mes3, anio3));
+        pacientesPorMes.set(NombreMes2, this.getFacade().CantidadPacientesPorMesTotal(mes2, anio2));
+        pacientesPorMes.set(NombreMes1, this.getFacade().CantidadPacientesPorMesTotal(mes1, anio1));
+
+        model.addSeries(pacientesPorMes);
+        model.getAxes().put(AxisType.X, new CategoryAxis("Meses"));
+
         return model;
     }
 }
